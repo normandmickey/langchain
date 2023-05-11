d696bbfeb33e8c327c82cdb7618312456e1a09b7
normandmickey <normandmickey@gmail.com>
2023-05-11 06:17:01 -0400

    Update attom_docs.py

--- langchain/chains/api/attom_docs.py
+++ langchain/chains/api/attom_docs.py
@@ -1,13 +1,11 @@
 # flake8: noqa
-ATTOM_DOCS = """API documentation:
-Endpoint: https://api.gateway.attomdata.com/propertyapi/v1.0.0/
+ATTOM_DOCS = """BASE URL: https://api.gateway.attomdata.com/propertyapi/v1.0.0/
 
-Use the /property/basicprofile resource to get basic property information and most recent transaction and taxes for a specific address.
+API Documentation
+The API endpoint /property/basicprofile accepts two parameters address1 and address2 and responds with a JSON object that contains basic property information. 
 
-There are only two request parameters in addition to the header.
-
-    address1 | string | The properties full street address including house number street name and apartment or unit number. 
-    address2 | string | The city, state part of the property address.
-    
+Parameter| Format | Required | Default Description
+address1 | string | Yes | The properties full street address including house number street name and apartment or unit number. 
+address2 | string | Yes | The city, state part of the property address.
 
 """
