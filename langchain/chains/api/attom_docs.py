5466958e9cc3881906e0a4b1eccbd6f5fc015c6c
normandmickey <normandmickey@gmail.com>
2023-05-10 23:50:59 -0400

    Update attom_docs.py

--- langchain/chains/api/attom_docs.py
+++ langchain/chains/api/attom_docs.py
@@ -12,6 +12,13 @@ There are only two request parameters in addition to the header.
 The Response JSON object contains an three object arrays. The first is object array is labeled "status" and the second object array labled "echoed_fields" and
 the third object array is labeled "property".
 Create and run the API call based on the address in the question to parse the resulting JSON objects and key pairs to find return the answer.  
+Here are some examples. 
+
+1. When was "62 Maple St, Canisteo NY" built? The API call would be 
+https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/basicprofile?address1=62%20Maple%20St&address2=Canisteo%20NY
+
+2. Who owns the house located at "12 E Academy St, Canisteo NY"? The API call would be 
+https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/basicprofile?address1=12%20E$20Academy%20St&address2=Canisteo%20NY
 
 
 """
