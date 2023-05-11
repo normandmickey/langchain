b3258d8ddbb6fe4c83cb23bb23d94cc21ef7a36b
normandmickey <normandmickey@gmail.com>
2023-05-11 06:43:13 -0400

    Update attom_docs.py

--- langchain/chains/api/attom_docs.py
+++ langchain/chains/api/attom_docs.py
@@ -3,11 +3,14 @@ ATTOM_DOCS = """BASE URL: https://api.gateway.attomdata.com/propertyapi/v1.0.0/
 
 API Documentation
 The API endpoint /property/basicprofile accepts two parameters address1 and address2 and responds with a JSON object that contains basic property information. 
-The JSON object includes the follwoing details. owner1 = owner, yearBuilt = year built
 
 Parameter| Format | Required | Default Description
 address1 | string | Yes | The properties full street address including house number street name and apartment or unit number. 
 address2 | string | Yes | The city, state part of the property address.
+
+Relevant Response key pairs
+owner1 | string | owner name
+yearBuilt | string | year built
 
 
 """
