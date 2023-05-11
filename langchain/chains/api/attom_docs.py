d354b62e4bc9a7e512b237b9f4ee0e1556ac8374
normandmickey <normandmickey@gmail.com>
2023-05-10 22:21:46 -0400

    Update attom_docs.py

--- langchain/chains/api/attom_docs.py
+++ langchain/chains/api/attom_docs.py
@@ -8,6 +8,8 @@ Request parameters
 
     address1 | string | The properties full street address including house number street name and apartment or unit number. 
     address2 | string | The city, state, zip part of the property address.
+    
+End of request parameters. Do not add any exclusions. 
 
 
 The Response JSON object contains an three object arrays. The first is object array is labeled "status" and the second object array labled "echoed_fields" and
