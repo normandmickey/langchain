a50b6d1ba404bfe92b2240b2e3a175ac85d59aea
normandmickey <normandmickey@gmail.com>
2023-05-11 06:07:24 -0400

    Update attom_docs.py

--- langchain/chains/api/attom_docs.py
+++ langchain/chains/api/attom_docs.py
@@ -9,14 +9,5 @@ There are only two request parameters in addition to the header.
     address1 | string | The properties full street address including house number street name and apartment or unit number. 
     address2 | string | The city, state part of the property address.
     
-Create the API call based on the address in the question. Here are some examples. 
 
-1. When was "62 Maple St, Canisteo NY" built? The API call would be 
-https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/basicprofile?address1=62%20Maple%20St&address2=Canisteo%20NY
-
-2. Who owns the house located at "12 E Academy St, Canisteo NY"? The API call would be 
-https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/basicprofile?address1=12%20E%20Academy%20St&address2=Canisteo%20NY
-    
-The Response JSON object contains an three object arrays. The first is object array is labeled "status" and the second object array labled "echoed_fields" and
-the third object array is labeled "property". 
 """
