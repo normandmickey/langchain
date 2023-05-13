627d86afeee925eb7b0ac9acae7ba018693b71f6
normandmickey <normandmickey@gmail.com>
2023-05-13 10:55:17 -0400

    Update attomv4_docs.py

--- langchain/chains/api/attomv4_docs.py
+++ langchain/chains/api/attomv4_docs.py
@@ -16,11 +16,4 @@ Parameter| Format | Required | Default Description
 name | string | Yes | This is the location name to search
 geographyTypeAbbreviation | string | Yes | Geography type abbreviation (default = PL)
 
-The API endpoint transaction/salestrend accepts only four parameters geoIdv4, startyear, endyear and interval and responds with a JSON object Get the average sale price, median sale price, and count of sales for the past 5 years in yearly intervals..
-Parameter| Format | Required | Default Description
-geoIdV4 | string | Yes | This is the specific geography code to search
-startyear | integer | Yes | The start year to search from
-endyear | integer | Yes | The end year to search from
-interval | string | Yes | The interval to search from (valid intervals are yearly, monthly and quarterly).
-
 """
