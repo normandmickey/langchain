5667ea3d64667047ed001c12d83bc46895627410
normandmickey <normandmickey@gmail.com>
2023-05-13 11:23:51 -0400

    Update attomv4_docs.py

--- langchain/chains/api/attomv4_docs.py
+++ langchain/chains/api/attomv4_docs.py
@@ -2,7 +2,7 @@
 ATTOMV4_DOCS = """BASE URL: https://api.gateway.attomdata.com/v4/
 
 API Documentation
-The API endpoint /location/lookup accepts only two parameters name and geographyTypeAbbreviation and responds with a JSON object that is used to identify applicable geocodes (geoIdV4)
+The API endpoint /location/lookup accepts only two parameters name and geographyTypeAbbreviation and responds with a JSON object that includes the geoIdV4 for the requested zip code.
 Parameter| Format | Required | Default Description
 name | string | Yes | This is the location name to search
 geographyTypeAbbreviation | string | Yes | Geography type abbreviation (default = ZI)
