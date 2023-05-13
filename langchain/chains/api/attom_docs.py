8a4212a980e91f17fd1fc9fc2860f3c6d4c7df27
normandmickey <normandmickey@gmail.com>
2023-05-13 15:25:14 -0400

    Update attom_docs.py

--- langchain/chains/api/attom_docs.py
+++ langchain/chains/api/attom_docs.py
@@ -23,6 +23,42 @@ heatingFuel | string | heating fuel
 heatingType | string | heating type
 sewerType | string | sewar type
 wallType | string | wall type
+saleSearchDate | date | sale search dat
+saleTransDate | date | sale transaction date
+saleRecDate | date | sale recorded date
+saleDisclosureType | string | sale disclosure type
+saleDocNum | string | sale document number
+saleTransType | string | sale transfer type 
+bldgSize |integer | square footage
+grossSizeAdjusted | integer | square footage adjusted 
+groundFloorSize | integer | ground floor square footage
+livingSize | integer | living space square footage
+bathsFull | integer | number of bath full rooms
+bathsTotal | integer | total number of bathrooms
+beds | integer | number of bedrooms
+roomsTotal | integer | total number of rooms
+condition | string | condition
+foundationType | string | foundation type
+garageType | string | garage type 
+prkgSize | string | parking lot size square feet
+prkgType | string | parking type
+levels | string | number of levels
+assdImprValue | integer | assessed value improvements 
+assdLandValue | integer | assessed value land
+assdTtlValue | integer | assessed value total
+mktImprValue | integer | market value improvements
+mktLandValue | integer | market value land
+mktTtlValue | integer | market value total
+taxAmt | float | tax amount
+taxYear | integer | tax year
+Homeowner | string | homeowner tax exemption
+improvementPercent | integer | improvement percent
+corporateIndicator | string | corporation indicator
+lastName | string | owner name
+absenteeOwnerStatus | string | absentee owner status
+mailingAddressOneLine | string | owner mailing address
+lastModified | date | last modified
+pubDate | date | published date
 
 The API endpoint /property/address accepts five parameters postalcode, propertytype, orderby, page, pagesize and responds with a JSON object that contains a list of properties within a zip code.
 Paramer| Format | Required | Default Description
